42271671e9e007965d9e5c3e4892864b27639f5f
Matthew Parker <mattparker124@gmail.com>
2022-04-26 03:33:25 -0500

    added post note saving

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { validateNote, createNewNote } = require('../../lib/notes');
+const notes = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+module.exports = router;
