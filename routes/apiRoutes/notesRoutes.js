b31be9b63bb300ef3188ba6ad9947d0a5a453ba7
Matthew Parker <mattparker124@gmail.com>
2022-04-26 03:41:14 -0500

    added test notes and updated notesRoutes to properly find notes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { validateNote, createNewNote } = require('../../lib/notes');
-const notes = require('../../db/db.json');
+const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     let results = notes;
